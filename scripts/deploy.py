9415ef4faac2972735f55004c88472d01d0b9fae
webelf101 <m63974529@gmail.com>
2022-01-04 04:27:48 +0800

    update fee level

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -7,4 +7,4 @@ def main():
     token0 = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
     token1 = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
     cellarTickInfo = [[0,-82030,-83310,1]]
-    CellarPoolShare.deploy(name, symbol, token0, token1, 3000, cellarTickInfo, {"from":acct})
+    CellarPoolShare.deploy(name, symbol, token0, token1, 500, cellarTickInfo, {"from":acct})
